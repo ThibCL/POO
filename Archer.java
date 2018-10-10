710814786378e8ea5c5514892ea7de2a7ecd2a5c
Thibault <thibault.cabourg@gmail.com>
2018-10-10 19:58:39 +0200

    Ajout premier fichier
    Faut commencer un jour

---
+++ Archer.java
@@ -0,0 +1,11 @@
+package org.centrale.projet.objet;
+
+/**
+ * Cette classe représente les personnages de type archer (sous-classe de Personnage)
+ * @author Mathilde
+ * 
+ */
+public class Archer extends Personnage {
+	//on fait juste un test
+}
+
